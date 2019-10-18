4d1fe668f31d0679155aead780b5ecd52baef0ab
Jacob Foster <jfost3@gmail.com>
2019-10-18 05:06:41 -0500

    Add JSDoc to api

--- api.js
+++ api.js
@@ -1,5 +1,17 @@
 const rp = require('request-promise-native');
 
+/**
+ * @typedef ChoonData
+ * @prop {boolean} success If the request was successful in getting a track item
+ * from Spotify via Choonz.
+ * @prop {boolean} [shouldTTS] Whether or not to format with TTS (if the song is
+ * currently playing).
+ * @prop {string} [track] Formatted track name and artist.
+ */
+
+/**
+ * @returns {ChoonData}
+ */
 async function getPlaying() {
 	const { item, is_playing } = await rp({
 		baseUrl: 'https://choonz.alca.tv/api/',
